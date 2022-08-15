6c9793d8fafd60feb1df49c8b9f7ceae2a9ce825
Vadim <uvolkov@374@gmail.com>
2022-08-15 22:58:15 +0300

    Fill first array

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,28 @@
 ﻿int firstSize = InputInt("What size of the string array? ");
 string[] magazine = new string[firstSize];
+FillStringArray(magazine);
+
+
+
+
+
+
+void FillStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = InputString("Enter element: ");
+    }
+}
+
+string InputString(string output)
+{
+    Console.WriteLine(output);
+    return Console.ReadLine();
+}
+
+int InputInt(string output)
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
